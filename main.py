ba90598f4d0b51cb50356e53af32247940048f5c
susan <susan.belling@peachpayments.com>
2023-03-17 14:07:13 +0200

    Space and click

--- main.py
+++ main.py
@@ -25,12 +25,11 @@ def next_card():
     canvas.itemconfig(card_title, text=language, fill="black")
     canvas.itemconfig(card_word, text=current_card[language], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-    flip_timer = window.after(3000, func=flip_card)
 
 
-def flip_card():
-    canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+def flip_card(e):
+    canvas.itemconfig(card_title, text="German", fill="white")
+    canvas.itemconfig(card_word, text=current_card["German"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
 
@@ -48,7 +47,7 @@ window = Tk()
 window.title("Study Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer = window.after(3000, func=flip_card)
+flip_timer = window.bind("<space>", func=flip_card) and window.bind("<Button-1>", func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 
@@ -57,8 +56,8 @@ card_back_img = PhotoImage(file="images/card_back.png")
 
 card_background = canvas.create_image(400, 263, image=card_front_img)
 
-card_title = canvas.create_text(400, 150, text="", fill="black", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "italic"))
+card_title = canvas.create_text(400, 50, text="", fill="black", font=("Ariel", 40, "italic"), width=650)
+card_word = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "italic"), width=650)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
